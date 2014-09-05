b60451b689b5d35d2e38b03fb7633b9249a6b715
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2014-09-05 16:32:57 +0200

    Update GoogleLogin.php
    add "Additional Configuration parameter"

--- extensions/GoogleLogin/GoogleLogin.php
+++ extensions/GoogleLogin/GoogleLogin.php
@@ -3,5 +3,8 @@ $wgGLSecret = '<your-client-secret>';
 $wgGLAppId = '<your-client-id>';
 
 $wgGLShowCreateReason = false;
-$wgGLAllowedDomains[gerrit 2] 	v0.1.1 	''
-$wgGLAllowedDomainsStrict[gerrit 2] 	v0.1.1 	false
+$wgGLAllowedDomains = '';
+$wgGLAllowedDomainsStrict = false;
+$wgGLShowKeepLogin = true;
+$wgGLAllowAccountCreation = $wgGroupPermissions['*']['createaccount'];
+$wgGLReplaceMWLogin = false;
