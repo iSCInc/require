f32ee99d3d7472545c5c2ed037331e1df5f73c5a
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2014-09-05 16:28:37 +0200

    Update GoogleLogin.php

--- extensions/GoogleLogin/GoogleLogin.php
+++ extensions/GoogleLogin/GoogleLogin.php
@@ -1,3 +1,7 @@
 require_once "$IP/extensions/GoogleLogin/GoogleLogin.php";
 $wgGLSecret = '<your-client-secret>';
 $wgGLAppId = '<your-client-id>';
+
+$wgGLShowCreateReason = false;
+$wgGLAllowedDomains[gerrit 2] 	v0.1.1 	''
+$wgGLAllowedDomainsStrict[gerrit 2] 	v0.1.1 	false
